5d3ecaeafd37741ef7f2788ac73f8ebb64a2a2f3
sagit <36596628+Sagit-chu@users.noreply.github.com>
2022-04-09 15:29:52 +0800

    definition branch

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,7 +12,7 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-echo 'src-git Sagit_Packages https://github.com/Sagit-chu/packages.git' >> feeds.conf.default
+echo 'src-git Sagit_Packages https://github.com/Sagit-chu/packages.git;main' >> feeds.conf.default
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
